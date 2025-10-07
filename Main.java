8934ca76e794d97c6b7096686a855c612d50dea8
sami mestar <samimestar123@gmail.com>
2025-10-07 18:51:45 +0200

    ajout de toString()

--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("bonjour");
+        Fraction f1=new Fraction();
+        System.out.println(f1.toString());
     }
 }
 class Fraction {
-
+    public String toString(){
+        return "Je suis une fraction.";
+    }
 }
